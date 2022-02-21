372f68287722160359b8149c53231902ddfaffa4
JuliaJia <juliajia25191@gmail.com>
2022-02-21 12:04:47 +0800

    add Test1 func

--- test01/test01.go
+++ test01/test01.go
@@ -1,7 +1,28 @@
 package test01
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func Test1() {
 	fmt.Println("我是来自github的Test1！")
+}
+
+func Test2() {
+	var (
+		host string
+		port int
+		h    bool
+		help bool
+	)
+	flag.StringVar(&host, "H", "127.0.0.1", "连接地址")
+	flag.IntVar(&port, "P", 22, "连接端口")
+	flag.BoolVar(&h, "h", false, "帮助")
+	flag.BoolVar(&help, "help", false, "帮助")
+	flag.Usage = func() {
+		fmt.Println("usage: testflag [-H 127.0.0.1] [-P 22]")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
 }
