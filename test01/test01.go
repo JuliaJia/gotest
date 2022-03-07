1c019e68fc7879166aa4bfba687dee0d361ef9d0
JuliaJia <juliajia25191@gmail.com>
2022-03-07 12:08:26 +0800

    modify ds print

--- test01/test01.go
+++ test01/test01.go
@@ -74,6 +74,6 @@ func TestHex() {
 	fmt.Printf("%x\n", []byte("我是林克！"))
 	fmt.Println(hex.EncodeToString([]byte("我是林克！")))
 	ds, _ := hex.DecodeString("e68891e698afe69e97e5858befbc81")
-	fmt.Println(ds)
+	fmt.Println(string(ds))
 
 }
